db25f1e59d9294a1b4967405df8fccc0f46a8782
CarlosTrejo2308 <cntrejo@prodigy.net.mx>
2018-05-02 13:41:12 -0600

    remove test from atom

--- src/seniorProject/Input_Tester.java
+++ src/seniorProject/Input_Tester.java
@@ -6,19 +6,17 @@ public class Input_Tester {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		
+
 		Input i = new Input("Input.txt");
-		
+
 		for (Course c: i.classesTaken){
 			System.out.println(c.getName());
 			System.out.println(c.getCH());
 			System.out.println(c.getPriority());
 			System.out.println(c.getStatus());
 			System.out.println(c.getGrade());
-			//Test
-			System.out.println("----");
 		}
-		
+
 	}
 
 }
